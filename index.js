c7d253e06fd5af7e2aba72eeb858710cc0e7fdb3
Diksha Tomar <Dikshatomar1511@gmail.com>
2022-06-08 00:34:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+var numberofDrumbuttons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberofDrumbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio1 = new Audio("sounds/kick-bass.mp3");
+                audio1.play();
+                break;
+            case "s":
+                var audio2 = new Audio("sounds/snare.mp3");
+                audio2.play();
+                break;
+            case "d":
+                var audio3 = new Audio("sounds/tom-1.mp3");
+                audio3.play();
+                break;
+            case "j":
+                var audio4 = new Audio("sounds/tom-2.mp3");
+                audio4.play();
+                break;
+            case "k":
+                var audio5 = new Audio("sounds/tom-3.mp3");
+                audio5.play();
+                break;
+            case "l":
+                var audio6 = new Audio("sounds/tom-4.mp3");
+                audio6.play();
+                break;
+        
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
